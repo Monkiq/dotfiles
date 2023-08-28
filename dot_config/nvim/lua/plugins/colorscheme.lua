562680b3fb33544a0b06839080b35d9d8853afe3
monkiq <monkiq@163.com>
2023-08-28 18:13:32 +0800

    LazyVim配置

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "gruvbox",
+      colorscheme = "catppuccin",
+    },
+  },
+}
