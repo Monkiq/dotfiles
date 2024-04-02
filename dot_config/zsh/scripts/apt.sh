fc91e0a8c958ac4ba3772212b13cf056805f0ad1
monkiq <monkiq@163.com>
2024-04-02 22:52:40 +0800

    Update .config/zsh/scripts/apt.sh

--- dot_config/zsh/scripts/apt.sh
+++ dot_config/zsh/scripts/apt.sh
@@ -1,1 +1,6 @@
-sudo apt-get install -y curl git mercurial make binutils bison gcc build-essential
+sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
+
+sudo apt update && apt-get install -y curl git mercurial make binutils bison gcc build-essential
+sudo apt-get install zsh
+
+sh -c "$(curl -fsLS get.chezmoi.io)" >/dev/null
