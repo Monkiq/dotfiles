074960cf067cd4ebf71b15c54ba5c11a4cdcfeea
monkiq <monkiq@163.com>
2024-04-02 11:13:33 +0800

    Update .config/zsh/aliases.sh

--- dot_config/zsh/aliases.sh
+++ dot_config/zsh/aliases.sh
@@ -3,5 +3,4 @@ if [ "$(uname)" = "Darwin" ]; then
 elif [ "$(uname)"="Linux" ]; then
 fi
 
-# alias kubectl="kubectl --kubeconfig /Users/monkiq/Projects/k8s/devcloud/monkiqchen_test/monkiqchen-test.conf"
 alias kk=kubectl
