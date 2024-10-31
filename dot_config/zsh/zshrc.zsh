1d128954695edb809442e6d93260faf06d435f1d
monkiq <monkiq@163.com>
2024-10-31 10:53:14 +0800

    update karabiner

--- dot_config/zsh/zshrc.zsh
+++ dot_config/zsh/zshrc.zsh
@@ -1,12 +1,10 @@
 export ZSH=$HOME/.config/zsh
 # 加载zinit插件
 source $ZSH/zinit.zsh
-
 # zsh 自动补全功能
 autoload -Uz compinit && compinit
 # zsh 颜色功能
 autoload -Uz colors && colors  # provide color variables (see `which colors`)
-
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
@@ -16,15 +14,8 @@ source ~/.config/zsh/aliases.sh
 source ~/.config/zsh/zsh-export.zsh
 source ~/.config/zsh/p10kconfig/p10k-rainbow.zsh
 
-eval "$(atuin init zsh)"
-
 export EDITOR='nvim'
 export VISUAL='nvim'
 
 
-eval $(thefuck --alias)
-g shell setup
-
-
-export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
 
