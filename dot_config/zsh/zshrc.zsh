341d4688b67b4c29fb4ef5c855395b3975d36658
monkiq <monkiq@163.com>
2024-11-11 16:12:05 +0800

    update zshrc

--- dot_config/zsh/zshrc.zsh
+++ dot_config/zsh/zshrc.zsh
@@ -5,13 +5,10 @@ source $ZSH/zim.zsh
 # zsh 颜色功能
 autoload -Uz colors && colors  # provide color variables (see `which colors`)
 
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
 source ~/.config/zsh/functions.sh
 source ~/.config/zsh/aliases.sh
 source ~/.config/zsh/zsh-export.zsh
-source ~/.config/zsh/p10kconfig/p10k-rainbow.zsh
 
 export EDITOR='nvim'
 export VISUAL='nvim'
